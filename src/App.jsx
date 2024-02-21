642f2a1962f3363483d7b72bd6fefdb8c86c3ff2
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-02-21 10:26:45 +0530

    cart management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-// import AddCartButton from './components/AddCartButton'
-// import MinusButton from './components/MinusButton'
-// import Classcomp from './components/Classcomp'
 import CartComponent from './components/CartComponent'
 import ButtonToCart from './components/ButtonToCart'
 import { Provider } from 'react-redux'
@@ -14,8 +8,6 @@ function App() {
   return (
     <>
     <Provider store={appStore}>
-
-     {/* {count==0?<AddCartButton setCount = {setCount}/>:<MinusButton />} */}
      <CartComponent />
      <ButtonToCart />
      </ Provider>
